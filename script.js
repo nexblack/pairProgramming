6a4a54c54203717f870c8a3eddc2107e352c1bb8
Yeahbuddy <yueah@thebest.com>
2021-10-24 17:28:36 -0400

    updated animation function

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function draw() {
   background(220);
   ellipse(x,50,80,80);
   x++;
+
+  if(x > 400) {
+    x = 5;
+  }
 }
 
-if(x > 400) {
-  x = 5;
-}
 
 
-console.log("version 1.2");
+console.log("version 1.3");
